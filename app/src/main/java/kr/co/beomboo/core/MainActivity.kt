90ac78540afcf43c14509ac6559feb5bb927b69b
beomboo <BeomBoo@gsitm.com>
2025-02-07 13:32:53 +0900

    push

--- app/src/main/java/kr/co/beomboo/core/MainActivity.kt
+++ app/src/main/java/kr/co/beomboo/core/MainActivity.kt
@@ -11,20 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.beomboo.core.ui.theme.CoreTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CoreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Greeting(
+                    name = "Android",
+                    modifier = Modifier.padding(innerPadding)
+                )
             }
         }
     }
@@ -41,7 +39,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    CoreTheme {
-        Greeting("Android")
-    }
+    Greeting("Android")
 }
--- core/src/main/java/kr/co/bkc/core/Utils.kt
+++ core/src/main/java/kr/co/bkc/core/Utils.kt
@@ -1,6 +1,6 @@
 package kr.co.bkc.core
 
-class Utils {
+object AndroidCoreUtils {
     fun printTestCode(){
         print("테스트입니당")
     }
